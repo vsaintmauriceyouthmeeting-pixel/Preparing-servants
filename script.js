ebe81a680bc6ce6d9c981362743b5780e51eded4
vsaintmauriceyouthmeeting-pixel <vsaintmauriceyouthmeeting@gmail.com>
2025-09-10 12:51:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDHcFkmP2rLgINNDIQfOrpKQxHKA7_0Q1A",
+  authDomain: "flutterattandaceforservice.firebaseapp.com",
+  projectId: "flutterattandaceforservice",
+  storageBucket: "flutterattandaceforservice.firebasestorage.app",
+  messagingSenderId: "10517178916",
+  appId: "1:10517178916:web:b0a0b2960b2bfadae106b1"
+};
